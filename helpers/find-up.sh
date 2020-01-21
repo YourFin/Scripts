b0e2bf34f5493243d6ba3aed638a8a292c214750
YourFin <d.junkpobox+git@gmail.com>
2020-01-21 15:03:23 -0800

    Fix find-up.sh to show all parent directories
    Also add more quotes, ignore current directory

--- helpers/find-up.sh
+++ helpers/find-up.sh
@@ -2,20 +2,22 @@
 # Needs to be passed to a function in bashrc or zshrc
 # in order to work
 
+# Returns the parents of a directory up to $HOME or /, whichever comes first
+# defined recursively
 function printParents() {
-    parent="$(dirname $1)"
-    echo -E "$(basename $1)"
-    [ "$parent" != "/" ] && [ "$parent" != "$HOME" ] \
+    parent="$(dirname "$1")"
+    echo -E "$(basename "$1")"
+    [ "$1" != "/" ] && [ "$1" != "$HOME" ] \
         && printParents "$parent"
 
 }
 function add_dash_e() {
-    for arg in $@ ; do
+    for arg in "$@" ; do
         echo -En "-e '${arg:Q}' "
     done
 }
 function get_candidate_rg() {
-    printParents "$(pwd)" \
+    printParents "$(dirname "$(pwd)")"  \
         | eval rg --no-config -I --smart-case \
                --sort none \
                --color never \
@@ -23,8 +25,14 @@ function get_candidate_rg() {
         | head -n 1
 }
 function get_candidate_fzf() {
-    cwd="$(pwd)"
-    printParents "$(pwd)" \
-        | fzf -q "$@" -1 -0
+    if [ "$#" -ne 0 ] ; then
+        printParents "$(dirname "$(pwd)")" \
+            | fzf -q "$@" -1 -0
+    else
+        printParents "$(dirname "$(pwd)")"  \
+            | fzf -1 -0
+    fi
 }
-pwd | sed "s:\(.*/$(get_candidate_fzf $@)/\).*:\1:"
+if type -t fzf 2>&1 1>/dev/null ; then
+    pwd | sed "s:\(.*/$(get_candidate_fzf $@)/\).*:\1:"
+fi
